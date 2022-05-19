847ad984433572071648e6e6a06cab4cb7175b28
link <link@qq.com>
2022-05-19 10:35:08 +0800

    first commit

---
+++ src/main/java/com/javalx/springcloud/MainTest.java
@@ -0,0 +1,12 @@
+package com.javalx.springcloud;
+
+/**
+ * @author javalx_kirito
+ * @version 1.0
+ * @create 2022-05-19 10:30
+ */
+public class MainTest {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
